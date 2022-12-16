f9d3326ac083862acd2324c34cf27e4fc145f17a
newt <axelskam.123@outlook.es>
2022-12-16 16:33:04 -0500

    initi calculator project

---
+++ src/main.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { CalculatorApp } from "./CalculatorApp";
+import "./index.scss";
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <CalculatorApp />
+  </React.StrictMode>
+);
